1b195a6952a76a987ed19aee0846e852b75b7caf
Zachary Brown <zachary.g.brown@gmail.com>
2025-07-09 15:44:22 -0700

    test: fixing bash quotes

--- tests/functional-tests.sh
+++ tests/functional-tests.sh
@@ -778,8 +778,8 @@ chia wallet show -f $txch_funds_fingerprint
 # Transfer funds to test wallet
 #chia wallet send -f $txch_funds_fingerprint -a 0.001 -t $test_wallet_address -m 0
 
-echo "Sending transaction to transfer 0.001 TXCH to test wallet"
-transaction_id=$(chia rpc wallet send_transaction '{"wallet_id": 1, "amount": 1000000000, "fee": 0, "memos":["transfer to test wallet"], "address": "${test_wallet_address}"}' | jq -r '.transaction_id')
+echo "Sending transaction to transfer 0.001 TXCH to test wallet ${test_wallet_address}"
+transaction_id=$(chia rpc wallet send_transaction "{\"wallet_id\": 1, \"amount\": 1000000000, \"fee\": 0, \"memos\":[\"transfer to test wallet\"], \"address\": \"$test_wallet_address\"}" | jq -r '.transaction_id')
 if [[ $? -ne 0 ]]; then
     fail_test "Failed to send transaction"
     return
